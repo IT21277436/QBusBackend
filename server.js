f0c490a0f9e33de0b88062d1dca74683e544b8da
IT21277436 <it21277436@my.sliit.lk>
2023-10-14 01:42:11 +0530

    LoginAndSignup complete

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const userRoutes = require('./routes/user')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoutes = require("./routes/user");
 
 // express app
-const app = express()
+const app = express();
+
+app.use(cors());
 
 // middleware
-app.use(express.json({limit: '5mb'}))
+app.use(express.json({ limit: "5mb" }));
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
