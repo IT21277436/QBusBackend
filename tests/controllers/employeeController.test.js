c6122a4aa0b9d40e2e1d82f1c8f293461d7fa749
it21215988 <it21215988@my.sliit.lk>
2023-10-21 19:56:26 +0530

    modifications

--- tests/controllers/employeeController.test.js
+++ tests/controllers/employeeController.test.js
@@ -18,16 +18,15 @@ describe('HTTP Employee Post', function () {
     };
 
     const res = await chai.request(serverURL)
-<<<<<<< Updated upstream
+
       .post('/api/employee')
-=======
-      .post('/api/employee') 
->>>>>>> Stashed changes
+
       .send(requestBody)
       .set('Content-Type', 'application/json');
 
     expect(res).to.have.status(200);
   });
+
   describe('HTTP Employee Get', function () {
     it('GET', async function () {
       const serverURL = 'http://localhost:4000'; 
@@ -36,48 +35,14 @@ describe('HTTP Employee Post', function () {
         .get('/api/employee') 
   
       expect(res).to.have.status(200);
-<<<<<<< Updated upstream
       
     });
   });
 
-  
-  describe('HTTP employee Post', function () {
-    it('POST', async function () {
-      const serverURL = 'http://localhost:4000'; 
-      const requestBody = {
-        "employeeId": "E00123",
-        "name": "waseem",
-        "nic": "200006544326",
-        "contactNumber": "0775677890",
-        "category": "Driver",
-        "region": "Galle"
-      };
-  
-      const res = await chai.request(serverURL)
-        .post('/api/employee') 
-        .send(requestBody)
-        .set('Content-Type', 'application/json');
-  
-      expect(res).to.have.status(200);
-     
-    });
-  });
-
-  
   describe('HTTP gorest Delete', function () {
     it('DELETE', async function () {
       const serverURL = 'http://localhost:4000'; 
       const employeeIdToDelete = 'E00123'; 
-=======
-   
-    });
-  });
-  describe('HTTP Employee Delete', function () {
-    it('DELETE', async function () {
-      const serverURL = 'http://localhost:4000'; 
-      const employeeIdToDelete = '652d6f53f5541d96aabde524'; 
->>>>>>> Stashed changes
   
       const res = await chai.request(serverURL)
         .delete(`/api/employee/${employeeIdToDelete}`) 
